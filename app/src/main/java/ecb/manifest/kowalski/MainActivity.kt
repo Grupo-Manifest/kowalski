ea20e5b9e6078a1615612f7d214dfc3876e4bf2b
Kaue Miziara <kauemiziara@gmail.com>
2023-10-14 17:46:55 -0300

    Implement first version of the drawer side menu

--- app/src/main/java/ecb/manifest/kowalski/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/MainActivity.kt
@@ -3,12 +3,42 @@ package ecb.manifest.kowalski
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DirectionsCar
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.QrCode
+import androidx.compose.material.icons.outlined.DirectionsCar
+import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material.icons.outlined.QrCode
+import ecb.manifest.kowalski.ui.navigation.DrawerNavigationItem
+import ecb.manifest.kowalski.ui.theme.ManifestKowalskiTheme
+import ecb.manifest.kowalski.ui.navigation.DrawerNavigationMenu
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ManifestKowalskiTheme {
+                val navigationItems = listOf(
+                    DrawerNavigationItem(
+                        title = "Map",
+                        selectedIcon = Icons.Filled.Place,
+                        unselectedIcon = Icons.Outlined.Place,
+                    ),
+                    DrawerNavigationItem(
+                        title = "Payment",
+                        selectedIcon = Icons.Filled.QrCode,
+                        unselectedIcon = Icons.Outlined.QrCode,
+                    ),
+                    DrawerNavigationItem(
+                        title = "Car Diagnostics",
+                        selectedIcon = Icons.Filled.DirectionsCar,
+                        unselectedIcon = Icons.Outlined.DirectionsCar,
+                    ),
+                )
 
+                DrawerNavigationMenu(navigationItems)
+            }
         }
     }
 }

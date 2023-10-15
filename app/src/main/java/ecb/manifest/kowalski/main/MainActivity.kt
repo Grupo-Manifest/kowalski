368848fa6aa0f0793daea49f10d85fb45abe7e78
Kaue Miziara <kauemiziara@gmail.com>
2023-10-14 22:30:47 -0300

    Add 'route' property to DrawerNavigationItem

--- app/src/main/java/ecb/manifest/kowalski/main/MainActivity.kt
+++ app/src/main/java/ecb/manifest/kowalski/main/MainActivity.kt
@@ -10,13 +10,16 @@ import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.material.icons.outlined.DirectionsCar
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material.icons.outlined.QrCode
+import androidx.compose.material3.ExperimentalMaterial3Api
 import dagger.hilt.android.AndroidEntryPoint
 import ecb.manifest.kowalski.main.ui.drawer_menu.DrawerNavigationItem
 import ecb.manifest.kowalski.main.ui.theme.ManifestKowalskiTheme
 import ecb.manifest.kowalski.main.ui.drawer_menu.DrawerNavigationMenu
+import ecb.manifest.kowalski.main.ui.navigation.MainScreenRoutes
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,20 +29,23 @@ class MainActivity : ComponentActivity() {
                         title = "Map",
                         selectedIcon = Icons.Filled.Place,
                         unselectedIcon = Icons.Outlined.Place,
+                        route = MainScreenRoutes.Rating.route,
                     ),
                     DrawerNavigationItem(
                         title = "Payment",
                         selectedIcon = Icons.Filled.QrCode,
                         unselectedIcon = Icons.Outlined.QrCode,
+                        route = MainScreenRoutes.Placeholder.route,
                     ),
                     DrawerNavigationItem(
                         title = "Car Diagnostics",
                         selectedIcon = Icons.Filled.DirectionsCar,
                         unselectedIcon = Icons.Outlined.DirectionsCar,
+                        route = MainScreenRoutes.Placeholder.route,
                     ),
                 )
 
-                DrawerNavigationMenu(navigationItems)
+                DrawerNavigationMenu(navigationItems = navigationItems)
             }
         }
     }
